bac4310c8d1869eaeae90beeb9859a495d6d6bbd
Guilherme Oliveira <htoguilherme@gmail.com>
2016-11-08 11:08:20 -0200

    Validação + Primeiro Form

--- AppLotis/AppLotis/MainPage.xaml.cs
+++ AppLotis/AppLotis/MainPage.xaml.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Text.RegularExpressions;
 
 namespace AppLotis {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+        }
+
+        /*private void PlacaTextChanged(object sender, TextChangedEventArgs e) {
+            EntryPlaca.TextChanged -= PlacaTextChanged;
+            if (e.NewTextValue.Length == 3 && e.OldTextValue != null && e.OldTextValue.Length == 2) {
+                EntryPlaca.Text = e.NewTextValue + "-";
+            }
+
+            if (e.NewTextValue.Length > 4 && !Regex.IsMatch(e.NewTextValue[e.NewTextValue.Length -1].ToString(), @"[0-9]")) {
+                EntryPlaca.Text = e.OldTextValue;
+            } else if (e.NewTextValue.Length < 4 && e.NewTextValue.Length != 0) {
+                if (!Regex.IsMatch(e.NewTextValue[e.NewTextValue.Length - 1].ToString(), @"[a-zA-Z]")) {
+                    EntryPlaca.Text = e.OldTextValue;
+                } else if (Regex.IsMatch(e.NewTextValue[e.NewTextValue.Length - 1].ToString(), @"[a-z]")) {
+                    var novoTexto = e.OldTextValue + e.NewTextValue[e.NewTextValue.Length - 1].ToString().ToUpper();
+                    EntryPlaca.Text = novoTexto;
+                }
+            }
+            Task.Yield();
+            EntryPlaca.TextChanged += PlacaTextChanged;
+        }*/
+
+        private void OnContinuarClicked(object sender, EventArgs e) {
+            
+
         }
     }
 }
---
+++ AppLotis/AppLotis/Validation/EmailValidatorBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppLotis.Validation {
+    public class NumericValidationBehavior : Behavior<Entry> {
+        protected override void OnAttachedTo(Entry entry) {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry) {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args) {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+
+    public class PlacaValidatorBehavior : Behavior<Entry> {
+        protected override void OnAttachedTo(Entry entry) {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry) {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e) {
+            ((Entry) sender).TextChanged -= OnEntryTextChanged;
+            /*if (e.OldTextValue != null && e.OldTextValue.Length == 3) {
+                ((Entry) sender).Text = e.NewTextValue + "-";
+            }*/
+            if (e.NewTextValue.Length > 7) {
+                ((Entry)sender).Text = e.OldTextValue;
+            }
+            if (e.NewTextValue.Length > 4 &&
+                !Regex.IsMatch(e.NewTextValue[e.NewTextValue.Length - 1].ToString(), @"[0-9]")) {
+                ((Entry) sender).Text = e.OldTextValue;
+            }
+            else if (e.NewTextValue.Length < 4 && e.NewTextValue.Length != 0) {
+                if (!Regex.IsMatch(e.NewTextValue[e.NewTextValue.Length - 1].ToString(), @"[a-zA-Z]")) {
+                    ((Entry) sender).Text = e.OldTextValue;
+                }
+                else if (Regex.IsMatch(e.NewTextValue[e.NewTextValue.Length - 1].ToString(), @"[a-z]")) {
+                    var novoTexto = e.OldTextValue + e.NewTextValue[e.NewTextValue.Length - 1].ToString().ToUpper();
+                    ((Entry) sender).Text = novoTexto;
+                }
+            }
+
+            ((Entry)sender).TextChanged += OnEntryTextChanged;
+        }
+    }
+}
