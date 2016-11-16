28c0038002dd2a8f2bb0c6a4a597eeca7afef834
Guilherme Oliveira <htoguilherme@gmail.com>
2016-11-16 11:39:35 -0200

    Agendamento com mapa

--- AppLotis/AppLotis/Pages/AgendamentoPage.xaml.cs
+++ AppLotis/AppLotis/Pages/AgendamentoPage.xaml.cs
@@ -6,12 +6,49 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
 
 namespace AppLotis.Pages {
     public partial class AgendamentoPage : ContentPage {
         public AgendamentoPage() {
             InitializeComponent();
             
+        }
+
+        protected override async void OnAppearing() {
+            var locator = CrossGeolocator.Current;
+            var posUsuario = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+            MapLocalizacao.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(posUsuario.Latitude, posUsuario.Longitude), Distance.FromKilometers(1)));
+            MapLocalizacao.Pins.Add(new Pin() {
+                Position = new Position(posUsuario.Latitude, posUsuario.Longitude),
+                Label = "Seu carro"
+            });
+        }
+
+        private void OnContinuarClicked(object sender, EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private async void OnBtnPesquisarClicked(object sender, EventArgs e) {
+            if (String.IsNullOrEmpty(EntryCep.Text)) {
+                await DisplayAlert("Erro", "O campo do cep deve ser preenchido", "Ok");
+                return;
+            }
+            Geocoder geocoder = new Geocoder();
+            var localizacoes = await geocoder.GetPositionsForAddressAsync(EntryCep.Text);
+            if (localizacoes.Any()) {
+                MapLocalizacao.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(localizacoes.FirstOrDefault().Latitude, localizacoes.FirstOrDefault().Longitude), Distance.FromKilometers(1)));
+                MapLocalizacao.Pins.RemoveAt(0);
+
+                var pinCarro = new Pin() {
+                    Position = new Position(localizacoes.FirstOrDefault().Latitude,
+                        localizacoes.FirstOrDefault().Longitude),
+                    Label = "Seu carro"
+                };
+
+                MapLocalizacao.Pins.Add(pinCarro);
+            }
         }
     }
 }
